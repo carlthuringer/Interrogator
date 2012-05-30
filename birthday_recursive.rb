21e80619d472419e0e0d5ea2e10b1353edb657ea
Carl Thuringer <carl@groupon.com>
2012-05-29 22:45:25 -0500

    First implementation of interrogation method.

--- birthday_recursive.rb
+++ birthday_recursive.rb
@@ -1,3 +1,8 @@
 class Birthday
-
+  def self.interrogate
+    year = ""
+    puts "What year were you born in?"
+    gets year
+    puts "So, you were born in 1970?"
+  end
 end
--- spec/birthday_recursive_spec.rb
+++ spec/birthday_recursive_spec.rb
@@ -1,7 +1,28 @@
 require './birthday_recursive.rb'
 
 describe Birthday do
+  before do
+    Birthday.stub!(:puts)
+    Birthday.stub!(:gets)
+  end
   it "exists" do
     Birthday
+  end
+
+  it "Asks you what year you were born in" do
+    Birthday.should_receive(:puts).with("What year were you born in?")
+    Birthday.interrogate
+  end
+
+  it "Requests some input for the year you were born in" do
+    Birthday.should_receive(:gets).and_return("1970")
+    Birthday.interrogate
+  end
+
+  it "Responds to the interrogation with statements." do
+    Birthday.should_receive(:gets).and_return("1970")
+    Birthday.should_receive(:puts).with("What year were you born in?")
+    Birthday.should_receive(:puts).once.with("So, you were born in 1970?")
+    Birthday.interrogate
   end
 end
