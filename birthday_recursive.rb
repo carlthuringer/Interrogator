f5d24349a83f48c4563dda17fad2618d0f9ce302
Carl Thuringer <carl@groupon.com>
2012-05-29 23:04:26 -0500

    Testing stdin and stdout with rspec is hard...

--- birthday_recursive.rb
+++ birthday_recursive.rb
@@ -1,8 +1,14 @@
 class Birthday
   def self.interrogate
     year = ""
+    month = ""
+    day = ""
     puts "What year were you born in?"
     gets year
-    puts "So, you were born in 1970?"
+    puts "What month were you born in?"
+    gets month
+    puts "What day were you born on?"
+    gets day
+    puts "So, you were born on #{month} #{day}, #{year}?"
   end
 end
--- spec/birthday_recursive_spec.rb
+++ spec/birthday_recursive_spec.rb
@@ -19,10 +19,29 @@ describe Birthday do
     Birthday.interrogate
   end
 
-  it "Responds to the interrogation with statements." do
+  it "Asks what month you were born in" do
+    Birthday.should_receive(:puts).at_least(:once).with("What month were you born in?")
+    Birthday.interrogate
+  end
+
+  it "Asks what day you were born on" do
+    Birthday.should_receive(:puts).at_least(:once).with("What day were you born on?")
+    Birthday.interrogate
+  end
+
+  it "Responds to the interrogation with statements" do
     Birthday.should_receive(:gets).and_return("1970")
-    Birthday.should_receive(:puts).with("What year were you born in?")
-    Birthday.should_receive(:puts).once.with("So, you were born in 1970?")
+    Birthday.should_receive(:gets).and_return("Oct")
+    Birthday.should_receive(:gets).and_return("5")
+    Birthday.should_receive(:puts).at_least(:once).with("So, you were born on Oct 5, 1970?")
+    Birthday.interrogate
+  end
+
+  it "Responds naturally when given different inputs" do
+    Birthday.should_receive(:gets).and_return("1983")
+    Birthday.should_receive(:gets).and_return("Oct")
+    Birthday.should_receive(:gets).and_return("4")
+    Birthday.should_receive(:puts).at_least(:once).with("So, you were born on Oct 4, 1983?")
     Birthday.interrogate
   end
 end
